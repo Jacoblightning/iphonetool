a1df61d0ea69f6eb155934c85cc90b0f3092246a
Jacob Freeman <t00niyf5@duck.com>
2026-07-27 08:02:48 -0400

    fix: Fix base dir for module/package layout

--- src/iphonetool/helpers.py
+++ src/iphonetool/helpers.py
@@ -84,4 +84,6 @@ def irecovery_command(cmd: str, ecid: Optional[int] = None) -> None:
 
 
 def base_directory() -> pathlib.Path:
-    return pathlib.Path(__file__).parent
+    script_dir = pathlib.Path(__file__).parent
+    base_dir = script_dir.parent.parent
+    return base_dir
